0e4472ab4787da726c1b6351890393860390ad12
coderSeth13 <sethnamara1313@gmail.com>
2025-03-08 03:08:50 +0300

    Implement site assets

---
+++ utils/images.ts
@@ -0,0 +1,89 @@
+export const images = {
+  // **Logos**
+  logos: {
+    white: "/logo-white.svg",
+    black: "/TME-Logo-New-2.svg",
+    bcg: "/bcg.jpg",
+    bnsf: "/bnsf.jpg",
+    cisco: "/CISCO.jpg",
+    deliotte: "/deloitte.jpg",
+    fb: "/fb.jpg",
+    genesys: "/genesys.jpg",
+    gfk: "/gfk.jpg",
+    harvard: "/HARVARD.jpg",
+    ipsos: "/ipsos.jpg",
+    kaplan: "/kaplan.jpg",
+    kmr: "/kmr.jpg",
+    medallia: "/medallia.jpg",
+    oracle: "/oracle.jpg",
+    pandora: "/pandora.jpg",
+    partners: "/partners.jpg",
+    salesforce: "/salesforce.jpg",
+  },
+
+  // **Icons**
+  icons: {
+    ai: "/AI-Datasets.svg",
+    asri: "/ASR.svg",
+    brain: "/brain-card.svg",
+    check: "/check.svg",
+    dataAnnotation: "/Data-Annotation.svg",
+    datePicker: "/datepicker.svg",
+    download: {
+      small: "/download(2).svg",
+      large: "/download.svg",
+    },
+    eduCard: "/edu-card.svg",
+    enterpriseCard: "/enterprise-card.svg",
+    info: "/info-icon-citation.svg",
+    humanEdited: "/Human-Edited.svg",
+    legal: {
+      png: "/legal-ico.svg",
+      jpeg: "/Legal.svg",
+    },
+    market: "/market-card.svg",
+    medical: {
+      jpeg: "/Medical.svg",
+      png: "/medical-card.svg",
+    },
+    spinner: "/spinner.svg",
+    star: "/Stars.svg",
+  },
+
+  // **Graphics / Backgrounds**
+  graphics: {
+    penBrushBg: "/pen-brushes.svg",
+  },
+
+  // **Hero & Banner Images**
+  heroImages: {
+    extraSmall: "/hero-new-300x287.png",
+    small: "/hero-new-768x734.png",
+    medium: "/hero-new-1024x979.png",
+    large: "/hero-new.png",
+  },
+
+  // **Feature Images**
+  accuracy: {
+    small: "/accuracy-1-300x258.png",
+    large: "/accuracy-1.png",
+  },
+  customize: {
+    small: "/customize-300x281.png",
+    large: "/customize.png",
+  },
+  enterprise: {
+    small: "/enterprise-291x300.png",
+    large: "/enterprise.png",
+  },
+  ellipse2: {
+    small: "/Ellipse-2-100x100.png",
+    large: "/Ellipse-2.png",
+  },
+  ellipse1: {
+    extraSmall: "/Ellipse-1-100x100.png",
+    small: "/Ellipse-20-1-140x140.png",
+    medium: "/Ellipse-20-1-150x150.png",
+    large: "/Ellipse-1.png",
+  },
+};
